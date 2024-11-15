34296f8de5ac695ee02c503973a401fa82c2b923
hope409 <ysh933@gmail.com>
2024-11-15 16:44:33 +0900

    feat:front-aiboard

---
+++ front/src/components/Board.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container p-1 flex">
+    <div class="card row col-3">
+      <img src="" alt="포스터">
+    </div>
+    <div class="card row col-9">
+      <p>원본 영화명</p>
+      <p>게시글 제목</p>
+      <p>게시글 내용</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .container {
+        display : flex;
+        justify-content: space-between;
+    }
+</style>
--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="nav-bar">
     <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/">AI 게시판</RouterLink> |
+    <RouterLink to="/aiboard">AI 게시판</RouterLink> |
     <RouterLink to="/">커뮤니티</RouterLink> |
     <RouterLink to="/">영화 정보</RouterLink>
   </nav>
