f039913d47cdb323661948d3b4427fbd0bda6dd1
hope409 <ysh933@gmail.com>
2024-11-15 16:53:27 +0900

    feat:front-community

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="nav-bar">
     <RouterLink to="/">Home</RouterLink> |
     <RouterLink to="/aiboard">AI 게시판</RouterLink> |
-    <RouterLink to="/">커뮤니티</RouterLink> |
+    <RouterLink to="/community">커뮤니티</RouterLink> |
     <RouterLink to="/">영화 정보</RouterLink>
   </nav>
 </template>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,7 @@ import LoginView from '@/views/LoginView.vue'
 import SignUpView from '@/views/SignUpView.vue'
 import ProfileView from '@/views/ProfileView.vue'
 import AiBoard from '@/views/AiBoard.vue'
+import CommunityView from '@/views/CommunityView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,7 +33,12 @@ const router = createRouter({
       path: '/aiboard',
       name: 'aiboard',
       component: AiBoard
-    }
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: CommunityView
+    },
   ],
 })
 
