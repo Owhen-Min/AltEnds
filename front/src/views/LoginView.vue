ed30289da4eb4a08b3fcf4fce2bd370a12e75044
hope409 <ysh933@gmail.com>
2024-11-15 15:22:09 +0900

    feat:login

---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form>
+      <label for="">아이디</label>
+      <input type="text"><br>
+
+      <label for="">패스워드</label>
+      <input type="text"><br>
+
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
