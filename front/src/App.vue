ab7c3937216c17c650d09e089983b2e5c85af0e6
Owhen Min <thwn911@gmail.com>
2024-11-15 15:00:28 +0900

    feat-navbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-
+    <NavBar />
+    <RouterView/>
   </div>
 </template>
 
 <script setup>
-
+import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <style scoped>
---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>이번주의 영화</h1>
+            <div v-for="movie in movies">
+                {{movie}}
+            </div>
+    </div>
+</template>
+
+<script setup>
+    const movies = ['리얼', '성냥팔이 소녀의 재림', '클레멘타인', '웃겨야 사는 영화']
+</script>
+
+<style scoped>
+
+</style>
