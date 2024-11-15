b17e498320376c7ca66fdbd7e99d3beaf22d0fa8
Owhen Min <thwn911@gmail.com>
2024-11-15 15:34:36 +0900

    feat-HomeView

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,10 +1,10 @@
 <template>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <div>
     <NavBar />
     <RouterView/>
   </div>
 </template>
-
 <script setup>
 import { RouterView } from 'vue-router';
 import NavBar from './components/NavBar.vue';
