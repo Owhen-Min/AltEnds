90cdbd2bd762259e864e65ed44f1df7b6c56cbf2
hope409 <ysh933@gmail.com>
2024-11-18 17:27:01 +0900

    feat-front-signup

--- front/src/stores/counter.js
+++ front/src/stores/counter.js
@@ -1,25 +1,71 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
+import { useRouter } from 'vue-router'
 
 
-// export const useMovieStore = defineStore('movie', () => {
-//   const movies = ref([])
-//   const API_URL = 'http://127.0.0.1:8000/api/v1'
-//   const getMovies = function () {
-//     axios({
-//       method: 'get',
-//       url: `${API_URL}/movies/`
-//     })
-//       .then((response) => {
-//         console.log('영화목록 가져오기 성공')
-//         console.log(response.data)
-//         movies.value = response.data
-//       })
-//       .catch((error) => {
-//         console.log('실패')
-//         console.log(error)
-//       })
-//   }
-//   return { movies, API_URL, getMovies, }
-// })
+export const useMovieStore = defineStore('movie', () => {
+  const API_URL = 'http://127.0.0.1:8000'
+  const token = ref(null)
+  const isLogin = computed(() => {
+    if (token.value === null) {
+      return false
+    } else {
+      return true
+    }
+  })
+  const router = useRouter()
+
+  const signUp = function (payload) {
+    const { username, password1, password2, firstname, nickname, email } = payload
+
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/signup/`,
+      data: {
+        username, password1, password2, firstname, nickname, email 
+      }
+    })
+      .then((response) => {
+        const password = password1
+        LoginView({ username, password })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const logIn = function (payload) {
+    const { username, password } = payload
+
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/login/`, 
+      data: {
+        username, password
+      }
+    })
+      .then((response) => {
+        token.value = response.data.key
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const logOut = function () {
+    axios({
+      method: 'post',
+      url: `${API_URL}/accounts/logout/`,
+    })
+      .then((response) => {
+        console.log(response.data)
+        token.value = null,
+        router.push({ name: 'HomeView' })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  return { API_URL, isLogin, signUp, logIn, logOut }
+})
