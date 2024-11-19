d44861caadae5565f63372f890f39b3ee28752da
hope409 <ysh933@gmail.com>
2024-11-19 17:23:44 +0900

    feat-likes

--- back/communityarticles/urls.py
+++ back/communityarticles/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('articles/', views.article_list),
     path('articles/<int:article_pk>/', views.article_detail),
+    path('articles/<int:article_pk>/likes', views.likes),
 ]
