02fbcce8e7adfe222885c8978a111e546bf55715
hope409 <ysh933@gmail.com>
2024-11-21 17:55:45 +0900

    design-logo

--- back/back/urls.py
+++ back/back/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.urls import path, include
 from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/v1/accounts/', include('dj_rest_auth.urls')),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('api/v1/accounts/<int:user_pk>/', views.GetProfile),
     path('api/v1/movies/', include('moviearticles.urls')),
     path('api/v1/communities/', include('communityarticles.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
