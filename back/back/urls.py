d3a290af2deeb9cd7e22926dd3e3cb890246b014
Owhen-Min <skyblue847@hanyang.ac.kr>
2024-11-21 15:47:23 +0900

    refactor:back/accounts_urls

--- back/back/urls.py
+++ back/back/urls.py
@@ -20,9 +20,9 @@ from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('dj_rest_auth.urls')),
-    path('accounts/signup/', include('dj_rest_auth.registration.urls')),
+    path('api/v1/accounts/', include('dj_rest_auth.urls')),
+    path('api/v1/accounts/signup/', include('dj_rest_auth.registration.urls')),
     path('api/v1/movies/', include('moviearticles.urls')),
     path('api/v1/communities/', include('communityarticles.urls')),
-    path('accounts/<int:user_pk>/', views.GetProfile)
+    path('api/v1/accounts/<int:user_pk>/', views.GetProfile),
 ]
